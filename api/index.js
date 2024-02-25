38ed8e6de60eb330f3480785162932acc1e8aa61
Abhishek Kushwaha <kabhishek5710@gmail.com>
2024-02-25 17:21:01 +0530

    Create a test API Route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import userRouter from "./Routes/user.route.js";
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -17,3 +18,5 @@ const app = express();
 app.listen(8080, () => {
   console.log("App is listening to port 8080");
 });
+
+app.use("/api/user", userRouter);
