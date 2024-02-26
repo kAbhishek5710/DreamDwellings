39907eb0abc7f9e08a38a4be6e6ad4f649b839df
Abhishek Kushwaha <kabhishek5710@gmail.com>
2024-02-27 00:04:57 +0530

    Created Signup api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 import userRouter from "./Routes/user.route.js";
+import authRouter from "./Routes/auth.route.js";
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -15,8 +16,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(8080, () => {
   console.log("App is listening to port 8080");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
