84aba6e9e54dc72f544671c28bb41fdc9af2264f
Abhishek Kushwaha <kabhishek5710@gmail.com>
2024-03-02 22:40:02 +0530

    Complete delete user functionality

--- api/Routes/user.route.js
+++ api/Routes/user.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { test } from "../Controllers/user.controller.js";
-import { updateUser } from "../Controllers/user.controller.js";
+import { updateUser, deleteUser } from "../Controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -11,6 +11,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } from "../../redux/user/userSlice";
 
 export default function Profile() {
@@ -84,6 +87,23 @@ export default function Profile() {
       dispatch(updateUserFailure(err.message));
     }
   };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (err) {
+      dispatch(deleteUserFailure(err.message));
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -144,10 +164,15 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete Account
+        </span>
         <span className="text-red-700 cursor-pointer">Log Out</span>
       </div>
-      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      {/* <p className="text-red-700 mt-5">{error ? error : ""}</p> */}
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User is updated Successfully" : ""}
       </p>
