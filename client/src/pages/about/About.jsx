9708928bef7cab67b2bc2314e44ef34112d9a074
Abhishek Kushwaha <kabhishek5710@gmail.com>
2024-03-15 15:19:41 +0530

    Complete about page

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Dream Dwellings
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Dream Dwellings" is a premier real estate website designed to assist
+        individuals and families in finding their ideal residential or
+        commercial properties. With a user-friendly interface and comprehensive
+        search functionalities, Dream Dwellings strives to simplify the process
+        of buying, selling, or renting real estate properties.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Dream Dwellings hosts an extensive database of property listings,
+        ranging from cozy apartments and suburban homes to luxurious estates and
+        commercial spaces. Users can browse through various categories,
+        including property type, location, price range, and more.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Registered users can create personalized accounts on Dream Dwellings to
+        save their favorite listings, set up property alerts, and track their
+        search history. This feature enhances the user experience by enabling
+        users to easily revisit properties of interest and stay updated on new
+        listings that match their preferences.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Dream Dwellings is committed to helping individuals and families find
+        their dream homes or investment properties with ease and confidence.
+        Whether you're a first-time homebuyer, seasoned investor, or property
+        owner looking to sell or rent, Dream Dwellings offers the tools,
+        resources, and expertise to support your real estate goals.
+      </p>
     </div>
-  )
+  );
 }
